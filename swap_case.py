dc40f8c5f2898477c08956fb525b5db6df868068
khalid6455 <105858714+khalid6455@users.noreply.github.com>
2022-09-24 23:12:24 -0700

    Create swap_case.py

---
+++ swap_case.py
@@ -0,0 +1,17 @@
+
+def swap_case(s):
+    modified_string = s.swapcase()
+    return modified_string
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
