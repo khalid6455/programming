0d86f01827866c06e19d2962a3a8162f3d74d4c8
khalid6455 <105858714+khalid6455@users.noreply.github.com>
2022-09-26 06:53:09 -0700

    Update angleMbc.py

--- angleMbc.py
+++ angleMbc.py
@@ -3,4 +3,4 @@ from math import *
 AB = int(input())
 BC = int(input())
 oposite = (sqrt(pow(AB,2)+pow(BC,2)))/2
-print(round(degrees(asin(oposite/BC)))+chr(176))
+print(str(round(degrees(asin(oposite/BC))))+chr(176))
