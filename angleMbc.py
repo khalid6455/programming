57a78e8f4dae1278e74c82fd43fb79569060c125
khalid6455 <105858714+khalid6455@users.noreply.github.com>
2022-09-26 06:48:09 -0700

    Create angleMbc.py

---
+++ angleMbc.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import *
+AB = int(input())
+BC = int(input())
+oposite = (sqrt(pow(AB,2)+pow(BC,2)))/2
+print(round(degrees(asin(oposite/BC)))+chr(176))
