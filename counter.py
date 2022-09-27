5c9771a600dc9dfdf6056f83e36926e6da28fe6d
khalid6455 <105858714+khalid6455@users.noreply.github.com>
2022-09-27 01:40:33 -0700

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,20 +1,17 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from collections import *
 X = int(input())
-list_size = input().split()
+list_size = Counter(input().split())
 
 N = int(input())
-lists = []
-for i in range (N):
-    value_size = input()
-    lists.append(value_size)
 total = 0
-for j in lists:
-    x = j.split()
+for i in range (N):
+    value_size = input().split()
+    size = value_size[0]
+    value =int( value_size[1])
+    if size in list_size and list_size[size]>0:
+        total += value
+        list_size[size] -= 1
     
-    if(x[0] in list_size ):
-        total += int(x[1])
-        list_size.remove(x[0])
-    else:
-        total =total
+
 print(total)
