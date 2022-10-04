a9a21b8a08584ae3f835b50b7c359a7fbe0465c4
khalid6455 <105858714+khalid6455@users.noreply.github.com>
2022-10-04 12:37:22 -0700

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps():
+    # Write your code here
+
+    if __name__ == '__main__':
+        n = int(input().strip())
+        a = list(map(int, input().rstrip().split()))
+    num_swap = 0
+    for i in range(n):
+        for j in range(n-1):
+            if(a[j] > a[j+1]):
+                a[j],a[j+1] = a[j+1],a[j]
+                
+                num_swap += 1
+    print('Array is sorted in '+ str(num_swap) +' swaps.')
+    print('First Element: '+str(a[0]))
+    print('Last Element: '+str(a[n-1]))
+
+countSwaps()
